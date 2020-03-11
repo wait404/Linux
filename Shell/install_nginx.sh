21127aee27d361d38aa3c97ddfdabcb8200ecbbb
wait404 <tencent@email.com>
2020-03-12 03:08:28 +0800

    install nginx script

---
+++ Shell/install_nginx.sh
@@ -0,0 +1,206 @@
+#!/usr/bin/env bash
+
+src_path=/usr/local/src
+nginx_init=/etc/init.d/nginx
+
+[ $EUID -ne 0 ] && echo "Please run as root." && exit 1
+
+if command -v apt-get &>/dev/null
+then
+    apt-get update
+    apt-get install gcc libpcre3 libpcre3-dev openssl libssl-dev zlib1g zlib1g-dev -y
+elif command -v yum &>/dev/null
+then
+    yum update
+    yum install gcc pcre pcre-devel openssl openssl-devel zlib zlib-devel -y
+else
+    echo "Please check your OS!"
+    exit 1
+fi
+read -p "Please input nginx version:" nginx_version
+if curl -Is http://nginx.org/download/nginx-${nginx_version}.tar.gz | grep 404 &> /dev/null
+then
+    echo "The nginx version is error!"
+    exit 1
+fi
+
+read -e -p "Please input nginx path(default path is /usr/local/nginx):" nginx_path
+if [ -z ${nginx_path} ]
+then
+    nginx_path=/usr/local/nginx
+else
+    mkdir -p ${nginx_path}
+fi
+
+function Install_http_substitutions()
+{
+    git clone https://github.com/yaoweibin/ngx_http_substitutions_filter_module.git ${src_path}/ngx_http_substitutions_filter_module
+}
+function Install_brotli()
+{
+    git clone https://github.com/google/ngx_brotli.git ${src_path}/ngx_brotli
+    cd ${src_path}/ngx_brotli
+    git submodule update --init
+}
+function Install_openssl()
+{
+    curl -sSL https://www.openssl.org/source/old/1.1.1/openssl-1.1.1.tar.gz -o ${src_path}/openssl-1.1.1.tar.gz
+    tar -zxf ${src_path}/openssl-1.1.1.tar.gz -C ${src_path}
+    mv ${src_path}/openssl-1.1.1 ${src_path}/openssl
+}
+function Install_nginx()
+{
+    curl -sSL http://nginx.org/download/nginx-${nginx_version}.tar.gz -o ${src_path}/nginx-${nginx_version}.tar.gz
+    tar -zxf ${src_path}/nginx-${nginx_version}.tar.gz -C ${src_path}
+    cd ${src_path}/nginx-${nginx_version}
+    ./configure --user=www --group=www --prefix=${nginx_path} --with-http_stub_status_module --with-http_ssl_module --with-http_v2_module --with-http_gzip_static_module --with-http_sub_module --with-stream --with-stream_ssl_module --add-module=../ngx_brotli --add-module=../ngx_http_substitutions_filter_module --with-openssl=../openssl
+    make -j `cat /proc/cpuinfo | grep -c processor` && make install
+    if [ $? -eq 0 ]
+    then
+        echo "Success!"
+    else
+        echo "Fail!"
+        exit 1
+    fi
+}
+function Install_nginx_init()
+{
+    if [ ! -f ${nginx_init} ]
+    then
+        cat > ${nginx_init} <<EOF
+#! /bin/sh
+
+### BEGIN INIT INFO
+# Provides:          nginx
+# Required-Start:    \$local_fs \$network \$remote_fs
+# Required-Stop:     \$local_fs \$network \$remote_fs
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: starts the nginx web server
+# Description:       starts nginx using start-stop-daemon
+### END INIT INFO
+
+NGINX_BIN='/usr/local/nginx/sbin/nginx'
+CONFIG='/usr/local/nginx/conf/nginx.conf'
+
+case "\$1" in
+    start)
+        echo -n "Starting nginx... "
+
+        PID=\$(ps -ef | grep "\$NGINX_BIN" | grep -v grep | awk '{print \$2}')
+        if [ "\$PID" != "" ]; then
+            echo "nginx (pid \$PID) already running."
+            exit 1
+        fi
+
+        \$NGINX_BIN -c \$CONFIG
+
+        if [ "\$?" != 0 ]; then
+            echo " failed"
+            exit 1
+        else
+            echo " done"
+        fi
+        ;;
+
+    stop)
+        echo -n "Stoping nginx... "
+
+        PID=\$(ps -ef | grep "\$NGINX_BIN" | grep -v grep | awk '{print \$2}')
+        if [ "\$PID" = "" ]; then
+            echo "nginx is not running."
+            exit 1
+        fi
+
+        \$NGINX_BIN -s stop
+
+        if [ "\$?" != 0 ] ; then
+            echo " failed. Use force-quit"
+            \$0 force-quit
+        else
+            echo " done"
+        fi
+        ;;
+
+    status)
+        PID=\$(ps -ef | grep "\$NGINX_BIN" | grep -v grep | awk '{print \$2}')
+        if [ "\$PID" != "" ]; then
+            echo "nginx (pid \$PID) is running..."
+        else
+            echo "nginx is stopped."
+            exit 0
+        fi
+        ;;
+
+    force-quit|kill)
+        echo -n "Terminating nginx... "
+
+        PID=\$(ps -ef | grep "\$NGINX_BIN" | grep -v grep | awk '{print \$2}')
+        if [ "\$PID" = "" ]; then
+            echo "nginx is is stopped."
+            exit 1
+        fi
+
+        kill \$PID
+
+        if [ "\$?" != 0 ]; then
+            echo " failed"
+            exit 1
+        else
+            echo " done"
+        fi
+        ;;
+
+    restart)
+        \$0 stop
+        sleep 1
+        \$0 start
+        ;;
+
+    reload)
+        echo -n "Reload nginx... "
+
+        PID=\$(ps -ef | grep "\$NGINX_BIN" | grep -v grep | awk '{print \$2}')
+        if [ "\$PID" != "" ]; then
+            \$NGINX_BIN -s reload
+            echo " done"
+        else
+            echo "nginx is not running, can't reload."
+            exit 1
+        fi
+        ;;
+
+    configtest)
+        echo -n "Test nginx configure files... "
+
+        \$NGINX_BIN -t
+        ;;
+
+    *)
+        echo "Usage: \$0 {start|stop|restart|reload|status|configtest|force-quit|kill}"
+        exit 1
+        ;;
+
+esac
+EOF
+        chmod +x ${nginx_init}
+    fi
+}
+Clean_files()
+{
+    rm -rf ${src_path}/openssl* ${src_path}/ngx_brotli ${src_path}/ngx_http_substitutions_filter_module ${src_path}/nginx-${nginx_version}*
+    if [ -f ${nginx_path}/sbin/nginx.old ]
+    then
+        rm -f ${nginx_path}/sbin/nginx.old
+    fi
+}
+
+Install_http_substitutions
+Install_brotli
+Install_openssl
+Install_nginx
+Install_nginx_init
+Clean_files
+${nginx_init} restart
+echo "The nginx version is:"
+${nginx_path}/sbin/nginx -v
