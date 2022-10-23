d5b3b3e8906a947ae60ce6a7d526945fb7011f86
root <root@loli.localdomain>
2022-10-23 16:34:42 +0800

    add nginx webdav

--- Shell/install_nginx.sh
+++ Shell/install_nginx.sh
@@ -8,10 +8,10 @@ nginx_service=/etc/systemd/system/nginx.service
 if command -v apt-get &> /dev/null
 then
     apt-get update
-    apt-get install curl gcc git libpcre3 libpcre3-dev make openssl libssl-dev zlib1g zlib1g-dev -y
+    apt-get install curl gcc git libxml2 libxml2-dev libxslt1.1 libxslt1-dev libpcre3 libpcre3-dev make openssl libssl-dev zlib1g zlib1g-dev -y
 elif command -v yum &> /dev/null
 then
-    yum install curl gcc git pcre pcre-devel make openssl openssl-devel zlib zlib-devel -y
+    yum install curl gcc git libxml2 libxml2-devel libxslt libxslt-devel pcre pcre-devel make openssl openssl-devel zlib zlib-devel -y
 else
     echo "Please check your OS!"
     exit 1
@@ -44,15 +44,19 @@ then
     mkdir -p ${nginx_path}
 fi
     
-function Install_http_substitutions()
+function Install_dav_ext()
 {
-    git clone https://github.com/yaoweibin/ngx_http_substitutions_filter_module.git ${src_path}/ngx_http_substitutions_filter_module
+    git clone https://github.com/arut/nginx-dav-ext-module.git ${src_path}/nginx-dav-ext-module
 }
 function Install_brotli()
 {
     git clone https://github.com/google/ngx_brotli.git ${src_path}/ngx_brotli
     cd ${src_path}/ngx_brotli
     git submodule update --init
+}
+function Install_http_substitutions()
+{
+    git clone https://github.com/yaoweibin/ngx_http_substitutions_filter_module.git ${src_path}/ngx_http_substitutions_filter_module
 }
 function Install_openssl()
 {
@@ -65,7 +69,7 @@ function Install_nginx()
     curl -sSL http://nginx.org/download/nginx-${nginx_version}.tar.gz -o ${src_path}/nginx-${nginx_version}.tar.gz
     tar -zxf ${src_path}/nginx-${nginx_version}.tar.gz -C ${src_path}
     cd ${src_path}/nginx-${nginx_version}
-    ./configure --user=${nginx_user} --group=${nginx_user} --prefix=${nginx_path} --with-http_stub_status_module --with-http_realip_module --with-http_ssl_module --with-http_v2_module --with-http_gzip_static_module --with-http_sub_module --with-stream --with-stream_ssl_module --with-stream_ssl_preread_module --add-module=../ngx_brotli --add-module=../ngx_http_substitutions_filter_module --with-openssl=../openssl
+    ./configure --user=${nginx_user} --group=${nginx_user} --prefix=${nginx_path} --with-http_dav_module --with-http_stub_status_module --with-http_realip_module --with-http_ssl_module --with-http_v2_module --with-http_gzip_static_module --with-http_sub_module --with-stream --with-stream_ssl_module --with-stream_ssl_preread_module --with-openssl=../openssl --add-module=../nginx-dav-ext-module --add-module=../ngx_brotli --add-module=../ngx_http_substitutions_filter_module
     make -j `cat /proc/cpuinfo | grep -c processor` && make install
     if [ $? -eq 0 ]
     then
@@ -105,15 +109,16 @@ EOF
 function Clean_files()
 {
     cd ${src_path}
-    rm -rf ${src_path}/openssl* ${src_path}/ngx_brotli ${src_path}/ngx_http_substitutions_filter_module ${src_path}/nginx-${nginx_version}*
+    rm -rf ${src_path}/openssl* ${src_path}/nginx-dav-ext-module ${src_path}/ngx_brotli ${src_path}/ngx_http_substitutions_filter_module ${src_path}/nginx-${nginx_version}*
     if [ -f ${nginx_path}/sbin/nginx.old ]
     then
         rm -f ${nginx_path}/sbin/nginx.old
     fi
 }
 
-Install_http_substitutions
+Install_dav_ext
 Install_brotli
+Install_http_substitutions
 Install_openssl
 Install_nginx
 Install_nginx_service
