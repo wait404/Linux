5647fe10d8725ee0f93f4c8f84ede9d497a32b7b
wait404 <25416181+wait404@users.noreply.github.com>
2024-11-30 21:47:33 +0800

    Update

--- Shell/install_nginx_with_webdav.sh
+++ Shell/install_nginx_with_webdav.sh
@@ -11,7 +11,7 @@ then
     apt-get install -y cmake curl g++ gcc git libbrotli-dev libpcre3 libpcre3-dev libssl-dev libxml2 libxml2-dev libxslt1.1 libxslt1-dev make ninja-build perl openssl zlib1g zlib1g-dev
 elif command -v yum &> /dev/null
 then
-    yum install -y brotli-devel cmake curl gcc git libxml2 libxml2-devel libxslt libxslt-devel make ninja-build pcre pcre-devel perl openssl openssl-devel zlib zlib-devel 
+    yum install -y brotli-devel cmake curl gcc gcc-c++ git libxml2 libxml2-devel libxslt libxslt-devel make ninja-build pcre pcre-devel perl openssl openssl-devel zlib zlib-devel 
 else
     echo "Please check your OS!"
     exit 1
