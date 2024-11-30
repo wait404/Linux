e7242e31cda5afd991e5b211b31b7cdad324873c
wait404 <25416181+wait404@users.noreply.github.com>
2024-11-30 21:44:47 +0800

    Update

--- Shell/install_nginx.sh
+++ Shell/install_nginx.sh
@@ -8,10 +8,10 @@ nginx_service=/etc/systemd/system/nginx.service
 if command -v apt-get &> /dev/null
 then
     apt-get update
-    apt-get install -y cmake curl gcc git libbrotli-dev libpcre3 libpcre3-dev libssl-dev libxml2 libxml2-dev libxslt1.1 libxslt1-dev make ninja-build perl openssl zlib1g zlib1g-dev
+    apt-get install -y cmake curl gcc git libbrotli-dev libpcre3 libpcre3-dev libssl-dev libxml2 libxml2-dev libxslt1.1 libxslt1-dev make perl openssl zlib1g zlib1g-dev
 elif command -v yum &> /dev/null
 then
-    yum install -y brotli-devel cmake curl gcc git libxml2 libxml2-devel libxslt libxslt-devel make ninja-build pcre pcre-devel perl openssl openssl-devel zlib zlib-devel 
+    yum install -y brotli-devel cmake curl gcc git libxml2 libxml2-devel libxslt libxslt-devel make pcre pcre-devel perl openssl openssl-devel zlib zlib-devel 
 else
     echo "Please check your OS!"
     exit 1
@@ -44,25 +44,24 @@ then
     mkdir -p ${nginx_path}
 fi
 
-function Install_boringssl()
-{
-    git clone https://boringssl.googlesource.com/boringssl ${src_path}/boringssl
-    cd ${src_path}/boringssl
-    cmake -GNinja -B build
-    ninja -C build
-}
 function Install_brotli()
 {
     git clone https://github.com/google/ngx_brotli.git ${src_path}/ngx_brotli
     cd ${src_path}/ngx_brotli
     git submodule update --init
 }
+function Install_openssl()
+{
+    curl -sSL https://github.com/openssl/openssl/releases/download/openssl-3.2.3/openssl-3.2.3.tar.gz -o ${src_path}/openssl-3.2.3.tar.gz
+    tar -zxf ${src_path}/openssl-3.2.3.tar.gz -C ${src_path}
+    mv ${src_path}/openssl-3.2.3 ${src_path}/openssl
+}
 function Install_nginx()
 {
     curl -sSL http://nginx.org/download/nginx-${nginx_version}.tar.gz -o ${src_path}/nginx-${nginx_version}.tar.gz
     tar -zxf ${src_path}/nginx-${nginx_version}.tar.gz -C ${src_path}
     cd ${src_path}/nginx-${nginx_version}
-    ./configure --user=${nginx_user} --group=${nginx_user} --prefix=${nginx_path}  --with-cc=c++ --with-debug --with-http_gunzip_module --with-http_gzip_static_module --with-http_realip_module --with-http_ssl_module --with-http_stub_status_module --with-http_v2_module --with-http_v3_module --with-cc-opt='-I../boringssl/include -x c' --with-ld-opt='-L../boringssl/build/ssl -L../boringssl/build/crypto' --with-stream --with-stream_realip_module --with-stream_ssl_module --with-stream_ssl_preread_module --add-module=../ngx_brotli
+    ./configure --user=${nginx_user} --group=${nginx_user} --prefix=${nginx_path} --with-http_gunzip_module --with-http_gzip_static_module --with-http_realip_module --with-http_ssl_module --with-http_stub_status_module --with-http_v2_module --with-http_v3_module --with-stream --with-stream_realip_module --with-stream_ssl_module --with-stream_ssl_preread_module --with-openssl=../openssl --add-module=../ngx_brotli
     make -j `cat /proc/cpuinfo | grep -c processor` && make install
     if [ $? -eq 0 ]
     then
@@ -100,15 +99,15 @@ EOF
 
 function Clean_files()
 {
-    rm -rf ${src_path}/boringssl ${src_path}/ngx_brotli ${src_path}/nginx-${nginx_version}*
+    rm -rf ${src_path}/ngx_brotli ${src_path}/openssl* ${src_path}/nginx-${nginx_version}*
     if [ -f ${nginx_path}/sbin/nginx.old ]
     then
         rm -f ${nginx_path}/sbin/nginx.old
     fi
 }
 
-Install_boringssl
 Install_brotli
+Install_openssl
 Install_nginx
 Install_nginx_service
 Clean_files
